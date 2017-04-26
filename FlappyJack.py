99e7050dc8027fa588bbbdcabfbfff12e9a366d6
Vivalize <Marko@markos-mbp.dynamic.ucsd.edu>
2017-04-25 20:42:42 -0700

    Got rid of old files and started over

---
+++ FlappyJack.py
@@ -0,0 +1,8 @@
+# FlappyJack Extension for Inkscape
+# Created by Andrew and Marko Ritachka on 4/25/17.
+
+class FlappyJack:
+	def __init__(self):
+		# Seperate vector image into individual layers
+		# Simplify layers by removing borders, etc
+		# Save document as a .tar file of all the layers
